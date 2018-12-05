691eab81c33c616933c7e775f178174e5bb6b725
AD\z5050155 <shelvin.chand@student.adfa.edu.au>
2018-12-05 19:15:04 +1100

    test

--- jsp.py
+++ jsp.py
@@ -69,6 +69,7 @@ def MinimalJobshopSat():
     status = solver.Solve(model)
 
     if status == cp_model.OPTIMAL:
+
         # Print out makespan.
         print('Optimal Schedule Length: %i' % solver.ObjectiveValue())
         print()
@@ -78,46 +79,6 @@ def MinimalJobshopSat():
             for task_id in range(0, len(jobs_data[job]) ):
                 print(str(task_id)+': Start: '+str(solver.Value(all_tasks[job,task_id].start))+ ' End:'+str(solver.Value(all_tasks[job,task_id].end)))
 
-        # Create one list of assigned tasks per machine.
-        # assigned_jobs = [[] for _ in all_machines]
-        # for job in all_jobs:
-        #     for task_id, task in enumerate(jobs_data[job]):
-        #         machine = task[0]
-        #         assigned_jobs[machine].append(
-        #             assigned_task_type(
-        #                 start=solver.Value(all_tasks[job, task_id].start),
-        #                 job=job,
-        #                 index=task_id))
-        #
-        # disp_col_width = 10
-        # sol_line = ''
-        # sol_line_tasks = ''
-        #
-        # print('Optimal Schedule', '\n')
-        #
-        # for machine in all_machines:
-        #     # Sort by starting time.
-        #     assigned_jobs[machine].sort()
-        #     sol_line += 'Machine ' + str(machine) + ': '
-        #     sol_line_tasks += 'Machine ' + str(machine) + ': '
-        #
-        #     for assigned_task in assigned_jobs[machine]:
-        #         name = 'job_%i_%i' % (assigned_task.job, assigned_task.index)
-        #         # Add spaces to output to align columns.
-        #         sol_line_tasks += name + ' ' * (disp_col_width - len(name))
-        #         start = assigned_task.start
-        #         duration = jobs_data[assigned_task.job][assigned_task.index][1]
-        #
-        #         sol_tmp = '[%i,%i]' % (start, start + duration)
-        #         # Add spaces to output to align columns.
-        #         sol_line += sol_tmp + ' ' * (disp_col_width - len(sol_tmp))
-        #
-        #     sol_line += '\n'
-        #     sol_line_tasks += '\n'
-        #
-        # print(sol_line_tasks)
-        # print('Task Time Intervals\n')
-        # print(sol_line)
 
 
 MinimalJobshopSat()
--- rcpsp.py
+++ rcpsp.py
@@ -7,51 +7,56 @@ from ortools.sat.python import cp_model
 
 
 def MinimalRCPSP():
-    """Minimal jobshop problem."""
+    """Minimal rcpsp problem."""
     # Create the model.
     model = cp_model.CpModel()
 
-    jobs_data = [  # task = (resource_req, processing_time).
-        [2, 3], [2, 2], [2, 2],[3,4] ];
-    job_pred=[[],[0],[0],[1,2]];
-
-    jobs_count = len(jobs_data)
-    all_jobs = range(jobs_count)
-    resources=1;
+    #define instance data
+    task_data = [  # task = (duration, resource_req).
+        #[3,2], [1, 2], [2, 2],[4,3] ];
+        [3,4],[6,3],[2,2],[4,3],[5,1],[3,2],[4,4],[1,6]]
+    task_pred=[
+        #[],[0],[0],[1,2]]; #predecessors
+        [],[],[0],[0],[0],[1],[2,3,4],[5]]
+    task_count = len(task_data)
+    all_tasks = range(task_count)
+    resourceCapacity=7;
 
     # Compute horizon.
-    horizon = 100#sum(task[1]  for task in jobs_data)
+    horizon = sum(task[0]  for task in task_data)
+
+    #Define decision variables
     task_type = collections.namedtuple('task_type', 'start end interval')
 
-    # Create jobs.
     all_tasks = {}
-    for taskid, task  in enumerate(jobs_data):
-        start_var = model.NewIntVar(1, horizon,'start_'+str(taskid))
-        duration = task[1]-1
-        end_var = model.NewIntVar(1, horizon, 'end_' +str(taskid))
+    for taskid, task  in enumerate(task_data):
+        start_var = model.NewIntVar(0, horizon,'start_'+str(taskid))
+        duration = task[0]
+        end_var = model.NewIntVar(0, horizon, 'end_' +str(taskid))
         interval_var = model.NewIntervalVar(start_var, duration, end_var, 'interval_'+str(taskid))
         all_tasks[taskid] = task_type(start=start_var, end=end_var, interval=interval_var)
 
+    #resource constraints
     intervals = []
     demands=[]
-
-    for task_id, task in enumerate(jobs_data):
-        intervals.append(all_tasks[task_id].interval)
-        demands.append(task[0])
-    model.AddCumulative(intervals,demands,3)
+    for taskid, task in enumerate(task_data):
+        intervals.append(all_tasks[taskid].interval)
+        demands.append(task[1])
+    model.AddCumulative(intervals,demands,resourceCapacity)
 
     # Add precedence contraints.
-    for taskid in all_jobs:
-        for pred_id in job_pred[taskid]:
-            model.Add(all_tasks[taskid].start > all_tasks[pred_id].end)
+    for taskid in all_tasks:
+        for pred_id in task_pred[taskid]:
+            model.Add(all_tasks[taskid].start >= all_tasks[pred_id].end)
 
-    obj_var = model.NewIntVar(1, horizon, 'makespan')
-
+    #Define objective function: minimize makespan
+    obj_var = model.NewIntVar(0, horizon, 'makespan')
     model.AddMaxEquality(
         obj_var,
-        [all_tasks[taskid].end for taskid in all_jobs])
+        [all_tasks[taskid].end for taskid in all_tasks])
     model.Minimize(obj_var)
 
+    #solve and disply result
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
 
@@ -60,8 +65,8 @@ def MinimalRCPSP():
         print('Optimal Schedule Length: %i' % solver.ObjectiveValue())
         print()
 
-        for taskid in all_jobs:
-            print(str(taskid) + ': Start: ' + str(solver.Value(all_tasks[taskid].start)) + ' End:' + str(
+        for taskid in all_tasks:
+            print(str(taskid+1) + ': Start: ' + str(solver.Value(all_tasks[taskid].start)) + ' End:' + str(
                 solver.Value(all_tasks[taskid].end)))
 
 MinimalRCPSP()
